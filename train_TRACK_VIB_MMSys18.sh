6a1b2a2aa978cc48b33bd9ea5593e125a9c458bd
Hugo Bell <s2124881@ed.ac.uk>
2021-09-12 14:23:40 +0100

    uploading additional material from submission to repo

--- train_TRACK_VIB_MMSys18.sh
+++
@@ -1,1 +0,0 @@
-python training_procedure_new.py -train -gpu_id 0 -dataset_name David_MMSys_18 -model_name TRACK_VIB -init_window 30 -m_window 5 -h_window 25 -exp_folder original_dataset_xyz -provided_videos
